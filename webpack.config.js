17f17317b57f63a31a798291162db1e4212f6c3f
Gleb Radutsky <gleb.radutsky@hearst.com>
2023-07-07 23:43:32 -0400

    created app from https://jsramblings.com/creating-a-react-app-with-webpack/

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'build')
+    },
+    port: 3000
+  },
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.(jsx?)$/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'build')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html')
+    })
+  ],
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  }
+}
